8f0efa53d263078da7df5cd1bb27a4c7b73b906e
behruzjon <baxtiyorovbehruzjon6789@gmail.com>
2025-12-04 14:08:32 +0500

    switched ngrok

--- src/client.js
+++ src/client.js
@@ -2,8 +2,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
-  // uri: 'https://radiative-marlin-unplayed.ngrok-free.dev/api',
-  uri: 'http://localhost:8000/api',
+  uri: 'https://radiative-marlin-unplayed.ngrok-free.dev/api',
+  // uri: 'http://localhost:8000/api',
 });
 
 const authLink = setContext((_, { headers }) => {
