20b63485f6769db925974c3c331903c7ded670f2
behruzjon <baxtiyorovbehruzjon6789@gmail.com>
2025-12-14 13:40:39 +0500

    changed url

--- src/client.js
+++ src/client.js
@@ -3,7 +3,7 @@ import { setContext } from '@apollo/client/link/context';
 import { useNavigate } from 'react-router-dom';
 
 const httpLink = new HttpLink({
-  url: 'https://food-backend-9r5x.onrender.com/',
+  url: 'https://food-backend-9r5x.onrender.com/api',
 });
 
 const authLink = setContext((_, { headers }) => {
