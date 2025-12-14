ace9e3407082afefba9f8990022e238d6888559e
behruzjon <baxtiyorovbehruzjon6789@gmail.com>
2025-12-14 13:42:33 +0500

    changed render url

--- src/client.js
+++ src/client.js
@@ -1,23 +1,20 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { useNavigate } from 'react-router-dom';
 
 const httpLink = new HttpLink({
-  url: 'https://food-backend-9r5x.onrender.com/api',
+  uri: 'https://food-backend-9r5x.onrender.com/api',
 });
 
 const authLink = setContext((_, { headers }) => {
-  if (localStorage.getItem('authStore')) {
-    const localToken = JSON.parse(localStorage.getItem('authStore')) || {};
-    const token = localToken?.state?.token;
+  const localToken = JSON.parse(localStorage.getItem('authStore')) || {};
+  const token = localToken?.state?.token;
 
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  }
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 export const clientAppollo = new ApolloClient({
